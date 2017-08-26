cf35a5707304ebb7adeecf67a2061950a77703fe
abhijith5 <abhijithchavarkkad@gmail.com>
2017-08-26 11:26:42 +0530

    Change HomePage

--- CityApp/Resources/Resource.Designer.cs
+++ CityApp/Resources/Resource.Designer.cs
@@ -60,6 +60,9 @@ namespace CityApp
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f050005
+			public const int button1 = 2131034117;
+			
 			// aapt resource value: 0x7f050004
 			public const int location = 2131034116;
 			
